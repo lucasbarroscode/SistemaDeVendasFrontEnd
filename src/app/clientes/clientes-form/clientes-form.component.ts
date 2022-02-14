78bc33dffc1058cdbed9cfb3a8be7f53bbdb036c
lucasbarroscode <lucasbabezerra@gmail.com>
2022-02-14 15:17:19 -0300

    Alerts para cadastro e erro de cadastro

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -13,6 +13,9 @@ export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
 
+  success: boolean = false;
+  errors: string[];
+
   constructor(private service: ClientesService) {
     this.cliente = new Cliente();
     
@@ -25,7 +28,12 @@ export class ClientesFormComponent implements OnInit {
   onSubmit(){
     this.service.salvar(this.cliente).subscribe(response => {
       console.log(response);
-    })
+        this.success = true;
+    }, errorResponse => {
+      this.errors = errorResponse.error.errors;
+      
+    }
+    )
   }
 
 }
