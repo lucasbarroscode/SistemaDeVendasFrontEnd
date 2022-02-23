fac9443c5231287e9482fcfb58f8f1b3f3c74d51
lucasbarroscode <lucasbabezerra@gmail.com>
2022-02-23 00:04:01 -0300

    corrigindo erro ao tentar carregar cliente por id e implementando atualizacao de cliente

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from '../cliente';
 
@@ -16,41 +17,57 @@ export class ClientesFormComponent implements OnInit {
 
   success: boolean = false;
   errors: string[];
-  id:number;
+  id: number;
 
-  constructor(private service: ClientesService, private router : Router, private activatedRoute: ActivatedRoute) {
+  constructor(private service: ClientesService, private router: Router, private activatedRoute: ActivatedRoute) {
     this.cliente = new Cliente();
-    
-   }
 
-  ngOnInit(): void {
-    let params = this.activatedRoute.params;
-    if(params && params.value && params.value.id){
-      this.id = params.value.id;
-
-      this.service.getClienteById(this.id).subscribe(response => this.cliente = response,
-        errorResponse => this.cliente = new Cliente()
-        )
-    }
   }
 
-  voltarParaListagem(){
+  ngOnInit(): void {
+    let params: Observable<Params> = this.activatedRoute.params;
+    params.subscribe(urlParams => {
+      this.id = urlParams['id'];
+      if(this.id){
+      this.service.getClienteById(this.id).subscribe(response => this.cliente = response,
+        errorResponse => this.cliente = new Cliente()
+      )
+      }
+    })
+
+
+  }
+
+  voltarParaListagem() {
     this.router.navigate(['/clientes-lista']);
   }
 
   //Data Bind que vem do template para o componente
-  onSubmit(){
-    this.service.salvar(this.cliente).subscribe(response => {
+  onSubmit() {
+
+    if (this.id) {
+
+      this.service.atualizar(this.cliente).subscribe(response => {
+        this.success = true;
+        this.errors = null;
+      }, errorResponse =>
+        this.errors = ['Erro ao atualizar o cliente'])
+
+    } else {
+
+      this.service.salvar(this.cliente).subscribe(response => {
         this.success = true;
         this.errors = null;
         this.cliente = response;
-    }, errorResponse => {
-      this.success = false;
-      this.errors = errorResponse.error.errors;
-      
-      
+      }, errorResponse => {
+        this.success = false;
+        this.errors = errorResponse.error.errors;
+      }
+      )
+
     }
-    )
+
+
   }
 
 }
