e7de5cbda59daabd43d634208ccfc0c4a9259dfb
lucasbarroscode <lucasbabezerra@gmail.com>
2022-03-02 11:41:42 -0300

    finalizando o cadastro de clientes

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -12,6 +12,8 @@ export class ClientesListaComponent implements OnInit {
 
   clientes: Cliente[] = [];
   clienteSelecionado: Cliente;
+  mensagemSucesso: string;
+  mensagemErro: string;
 
   constructor(private service: ClientesService, private router :Router) {
 
@@ -27,6 +29,16 @@ export class ClientesListaComponent implements OnInit {
 
   preparaDelecao(cliente: Cliente){
     this.clienteSelecionado = cliente;
+  }
+
+  deletarCliente(){
+    this.service.deletar(this.clienteSelecionado)
+    .subscribe(
+    response => {
+      this.mensagemSucesso = 'Cliente deletado com sucesso!'
+      this.ngOnInit();
+    },
+    erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente!')
   }
 
 }
