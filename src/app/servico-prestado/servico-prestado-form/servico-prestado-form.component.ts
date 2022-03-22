96651c3c4ede36d518e0a61eb05dc17568cb3f79
lucasbarroscode <lucasbabezerra@gmail.com>
2022-03-22 16:15:21 -0300

    formulario de servico prestado

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from 'src/app/clientes.service';
+import { Cliente } from 'src/app/clientes/cliente';
 
 @Component({
   selector: 'app-servico-prestado-form',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoFormComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente[] = []
+
+  constructor(private clienteService: ClientesService) { }
 
   ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(resposta => this.clientes = resposta);
+
+  }
+  
+  onSubmit(){
+    console.log('Submit');
   }
 
 }
