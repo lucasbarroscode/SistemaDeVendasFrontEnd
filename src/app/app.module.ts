e84cf149f157ef7e95a20adfcff1250cc0134d7b
lucasbarroscode <lucasbabezerra@gmail.com>
2022-03-31 17:09:03 -0300

    criando o service de servicosPrestado e declarando a url na variavel de ambiente utilizando environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component'
 import { ClientesModule } from './clientes/clientes.module';
 import {ClientesService} from './clientes.service'
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
+import { ServicoPrestadoService } from './servico-prestado.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.modul
   ],
   //serve para injetar nos componentes
   providers: [
-    ClientesService
+    ClientesService,
+    ServicoPrestadoService
   ],
   bootstrap: [AppComponent]
 })
