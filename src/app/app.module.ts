8ed406f56dc591f4f07d4cde1e12a95e0e083888
lucasbarroscode <lucasbabezerra@gmail.com>
2022-04-10 16:36:30 -0300

    criacao do formulario de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,19 +11,24 @@ import { ClientesModule } from './clientes/clientes.module';
 import {ClientesService} from './clientes.service'
 import { ServicoPrestadoModule } from './servico-prestado/servico-prestado.module';
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
     ServicoPrestadoModule
+    
     
   ],
   //serve para injetar nos componentes
