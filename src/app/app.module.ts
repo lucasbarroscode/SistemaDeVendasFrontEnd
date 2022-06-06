c625e7de0b975520d2ef625ac8c73ba3eb8b3bc2
lucasbarroscode <lucasbabezerra@gmail.com>
2022-06-06 00:18:00 -0300

    cadastrando usuario no front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ServicoPrestadoService } from './servico-prestado.service';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { LayoutComponent } from './layout/layout.component';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { LayoutComponent } from './layout/layout.component';
   //serve para injetar nos componentes
   providers: [
     ClientesService,
-    ServicoPrestadoService
+    ServicoPrestadoService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -12,8 +14,9 @@ export class LoginComponent  {
   password:string;
   loginError:boolean;
   cadastrando:boolean;
+  mensagemSucesso:string;
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, private authService : AuthService) { }
 
   onSubmit(){
     this.router.navigate(['/home']);
@@ -26,6 +29,20 @@ export class LoginComponent  {
 
   cancelaCadastro(){
     this.cadastrando = false;
+  }
+
+  cadastrar(){
+    const usuario: Usuario = new Usuario();
+    usuario.username = this.username;
+    usuario.password = this.password;
+    this.authService.salvar(usuario).subscribe(response => {
+        this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login";
+        this.loginError = false;
+      }, error => {
+        this.loginError = true;
+        this.mensagemSucesso = null;
+
+      })
   }
 
 }
