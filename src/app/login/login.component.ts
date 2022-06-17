250fd014aa73eea504813b6bf54b2e3014a0fa96
lucasbarroscode <lucasbabezerra@gmail.com>
2022-06-17 00:40:08 -0300

    algumas melhorias no formulario de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -37,6 +37,10 @@ export class LoginComponent  {
     usuario.password = this.password;
     this.authService.salvar(usuario).subscribe(response => {
         this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login";
+        this.cadastrando = false;
+        this.username = '';
+        this.password = '';
+        this.errors = [];
       }, errorResponse => {
         this.mensagemSucesso = null;
         this.errors = errorResponse.error.errors;
